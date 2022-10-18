64d9ebe96da3260cc3af2f2ecce0d17e811f0625
Neil Cruz <neilbcruz@gmail.com>
2022-10-18 18:27:58 -0400

    add form path and folders/finish main edit inv

--- src/pages/EditWarehouse/EditInventory.js
+++ src/pages/EditWarehouse/EditInventory.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import ReturnArrow from '../../assets/images/icons/arrow_back-24px.svg';
+import EditInventoryForm from '../../components/EditInventoryForm/EditInventoryForm';
 
-function EditInventory() {
+export default function EditInventory() {
     return (
         <>
-        <div className='edit__inv'>
-            
+        <div className='editinv'>
+            <div className='editinv__header'>
+                <Link to='/'>
+                    <img 
+                    src={ReturnArrow}
+                    className='editinv__header-img'
+                    alt='Go back arrow'
+                    />
+                </Link>
+                <h1 className='editinv__title'>
+                    Edit Inventory Item
+                </h1>
+            </div>
+            <div className='editinv__form'>
+                <EditInventoryForm />
+            </div>
         </div>
         </>
     )
