f5b5933d4cd6797866ff28da9247f91e534286bb
timohuennebeck <hello@timohuennebeck.com>
2022-10-18 23:35:15 +0200

    add files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import PageHeader from './components/PageHeader/PageHeader';
+// import WarehouseList from './pages/WarehouseList/WarehouseList';
+// import WarehouseDetail from './pages/WarehouseDetail/WarehouseDetail';
+// import InventoryList from './pages/InventoryList/InventoryList';
+// import InventoryDetail from './pages/InventoryDetail/InventoryDetail';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* <Router>
+        <PageHeader />
+        <Routes>
+          <Route path='/' element={<WarehouseList />} />
+          <Route path='/warehouses' element={<WarehouseList />} />
+          <Route path='/inventory' element={<InventoryList />} />
+          <Route path='/warehouses/:id' element={<WarehouseDetail />} />
+          <Route part='inventory/:id' element={<InventoryDetail />} />
+        </Routes>
+      </Router> */}
     </div>
   );
 }
