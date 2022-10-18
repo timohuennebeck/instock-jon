10252e8dce153f24d12f17aa31699b9118cc9c95
timohuennebeck <hello@timohuennebeck.com>
2022-10-19 00:10:02 +0200

    add warehouse details page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import './App.scss';
-// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 // import PageHeader from './components/PageHeader/PageHeader';
 // import WarehouseList from './pages/WarehouseList/WarehouseList';
-// import WarehouseDetail from './pages/WarehouseDetail/WarehouseDetail';
+import WarehouseDetails from './pages/WarehouseDetails/WarehouseDetails';
 // import InventoryList from './pages/InventoryList/InventoryList';
 // import InventoryDetail from './pages/InventoryDetail/InventoryDetail';
 
 function App() {
   return (
     <div>
-      {/* <Router>
-        <PageHeader />
+      <Router>
+        {/* <PageHeader /> */}
         <Routes>
-          <Route path='/' element={<WarehouseList />} />
+          {/* <Route path='/' element={<WarehouseList />} />
           <Route path='/warehouses' element={<WarehouseList />} />
-          <Route path='/inventory' element={<InventoryList />} />
-          <Route path='/warehouses/:id' element={<WarehouseDetail />} />
-          <Route part='inventory/:id' element={<InventoryDetail />} />
+          <Route path='/inventory' element={<InventoryList />} /> */}
+          <Route path='/warehouses/:id' element={<WarehouseDetails />} />
+          {/* <Route part='inventory/:id' element={<InventoryDetail />} /> */}
         </Routes>
-      </Router> */}
+      </Router>
     </div>
   );
 }
---
+++ src/pages/WarehouseDetails/WarehouseDetails.jsx
@@ -0,0 +1,7 @@
+import "./WarehouseDetails.scss";
+
+function WarehouseDetails() {
+    return <div>WarehouseDetails</div>;
+}
+
+export default WarehouseDetails;
