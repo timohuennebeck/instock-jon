6153e096462a7415c6af8d6070587b1357587474
Neil Cruz <neilbcruz@gmail.com>
2022-10-18 18:08:11 -0400

    add asset folder plus first edit ware commit

---
+++ src/pages/EditWarehouse/EditInventory.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import ReturnArrow from '../../assets/images/icons/arrow_back-24px.svg';
+
+function EditInventory() {
+    return (
+        <>
+        <div className='edit__inv'>
+            
+        </div>
+        </>
+    )
+}
