b599d98c934745887d684c971302b680a8c4529f
Chathurika <chathurika-jayakody.com>
2022-10-18 23:21:27 -0400

    Add warehouse cards to the list

---
+++ src/components/WarehouseCard/WarehouseCard.js
@@ -0,0 +1,36 @@
+import './WarehouseCard.scss'
+import deleteIcon from '../../assets/images/icons/delete_outline-24px.svg'
+import editIcon from '../../assets/images/icons/edit-24px.svg'
+
+export default function WarehouseCard() {
+    return (
+        <div className='warehouse-card'>
+            <div className='warehouse-card__container'>
+                <div className='warehouse-card__tile'>
+                    <p className='p-small warehouse-card__title'>WAREHOUSE</p>
+                    <p className='p-medium warehouse-card__content warehouse-card__name'>Manhattan</p>
+                </div>
+                <div className='warehouse-card__tile'>
+                    <p className='p-small warehouse-card__title'>ADDRESS</p>
+                    <p className='p-medium warehouse-card__content '>503 Broadway, New York, USA</p>
+                </div>
+            </div>
+
+            <div className='warehouse-card__container'>
+                <div className='warehouse-card__tile'>
+                    <p className='p-small warehouse-card__title'>CONTACT NAME</p>
+                    <p className='p-medium  warehouse-card__content '>Parmin Aujla</p>
+                </div>
+                <div className='warehouse-card__tile warehouse-card__tile--contact'>
+                    <p className='p-small warehouse-card__title'>CONTACT INFORMATION</p>
+                    <p className='p-medium  warehouse-card__content'>+1(629)555-0129</p>
+                    <p className='p-medium  warehouse-card__content'>paujla@instock.com</p>
+                </div>
+            </div>
+            <div className='warehouse-card__icons-container'>
+                <img className='warehouse-card__icons'src={deleteIcon} alt='Delete Warehouse'></img>
+                <img className='warehouse-card__icons'src={editIcon} alt='Edit Warehouse'></img>
+            </div>
+        </div>
+    )
+}
--- src/components/WarehouseList/WarehouseList.js
+++ src/components/WarehouseList/WarehouseList.js
@@ -1,15 +1,33 @@
-import WarehouseForm from '../WarehouseForm/WarehouseForm'
-import './WarehouseList.scss'
+import WarehouseCard from "../WarehouseCard/WarehouseCard";
+import WarehouseForm from "../WarehouseForm/WarehouseForm";
+import WarehouseListHeader from "../WarehouseListHeader/WarehouseListHeader";
+import "./WarehouseList.scss";
 
-export default function WarehouseList(){
+export default function WarehouseList() {
+  // props: warehousedata
 
-    // props: warehousedata
-    
-    return (
-        <div className='warehouse'>
-            <h1 className='warehouse__title'>Warehouses</h1>
-            <WarehouseForm />
-            {/* list */}
-        </div>
-    )
+  return (
+    <>
+    <div className="warehouse">
+      <div className="warehouse__header">
+        <h1 className="warehouse__header__title">Warehouses</h1>
+        <WarehouseForm />
+      </div>
+      <WarehouseListHeader />
+      {/* list */}
+      <WarehouseCard />
+      <WarehouseCard />
+      {/* {warehouseData.map((warehouse) => (
+        <WarehouseListCard
+          key={warehouseData.id}
+          name={warehouseData.name}
+          address={warehouseData.address}
+          contactName={warehouseData.contact.name}
+          phone={warehouseData.contact.phone}
+          email={warehouseData.contact.email}
+        />
+      ))} */}
+    </div>
+    </>
+  );
 }
