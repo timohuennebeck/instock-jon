9e6cd422898798a047a2f0fd2e9fa4b349d80c8e
timohuennebeck <hello@timohuennebeck.com>
2022-10-19 15:00:59 +0200

    add transparent background to delete notification

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.scss';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import WarehousesDetailsEdit from './pages/WarehousesDetailsEdit/WarehousesDetailsEdit';
 import WarehousesDetailsAdd from './pages/WarehouseDetailsAdd/WarehousesDetailsAdd';
+import DeleteNotification from './components/DeleteNotification/DeleteNotification';
 // import PageHeader from './components/PageHeader/PageHeader';
 // import WarehouseList from './pages/WarehouseList/WarehouseList';
 // import WarehouseDetails from './pages/WarehouseDetails/WarehouseDetails';
@@ -12,14 +13,15 @@ function App() {
   return (
     <div>
       <Router>
+        <DeleteNotification />
         {/* <PageHeader /> */}
         <Routes>
           {/* <Route path='/' element={<WarehouseList />} />
           <Route path='/warehouses' element={<WarehouseList />} />
           <Route path='/inventory' element={<InventoryList />} /> */}
           {/* <Route path='/warehouses/:id' element={<WarehouseDetails />} /> */}
-          <Route path="warehouses/:id/edit" element={<WarehousesDetailsEdit />}/>
-          <Route path="warehouses/add" element={<WarehousesDetailsAdd />}/>
+          {/* <Route path="warehouses/:id/edit" element={<WarehousesDetailsEdit />}/>
+          <Route path="warehouses/add" element={<WarehousesDetailsAdd />}/> */}
           {/* <Route part='inventory/:id' element={<InventoryDetail />} /> */}
         </Routes>
       </Router>
