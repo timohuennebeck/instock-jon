78ccaf25389df8835467753538b6afc4d5fa30e9
Neil Cruz <neilbcruz@gmail.com>
2022-10-19 18:33:23 -0400

    add new page and scss

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import './App.scss';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// import WarehousesDetailsEdit from './pages/WarehousesDetailsEdit/WarehousesDetailsEdit';
+import WarehousesDetailsEdit from './pages/WarehousesDetailsEdit/WarehousesDetailsEdit';
 import WarehousesDetailsAdd from './pages/WarehouseDetailsAdd/WarehousesDetailsAdd';
-import DeleteNotification from './components/DeleteNotification/DeleteNotification';
+// import DeleteNotification from './components/DeleteNotification/DeleteNotification';
 // import PageHeader from './components/PageHeader/PageHeader';
 // import WarehouseList from './pages/WarehouseList/WarehouseList';
 // import WarehouseDetails from './pages/WarehouseDetails/WarehouseDetails';
 // import InventoryList from './pages/InventoryList/InventoryList';
 // import InventoryDetail from './pages/InventoryDetail/InventoryDetail';
 import EditInventory from './pages/EditInventory/EditInventory';
+import AddInventory from './pages/AddInventory/AddInventory';
 
 function App() {
   return (
@@ -17,12 +18,14 @@ function App() {
         {/* <DeleteNotification /> */}
         {/* <PageHeader /> */}
         <Routes>
-          {/* <Route path='/' element={<WarehouseList />} />
-          <Route path='/warehouses' element={<WarehouseList />} />
-          <Route path='/inventory' element={<InventoryList />} /> */}
+          {/* <Route path='/' element={<WarehouseList />} /> */}
+          {/* <Route path='/warehouses' element={<WarehouseList />} /> */}
+          {/* <Route path='/inventory' element={<InventoryList />} /> */}
+          <Route path='/inventory/edit' element={<EditInventory />} />
+          <Route path='/inventory/add' element={<AddInventory />} />
           {/* <Route path='/warehouses/:id' element={<WarehouseDetails />} /> */}
-          {/* <Route path="warehouses/:id/edit" element={<WarehousesDetailsEdit />}/> */}
-          {/* <Route path="warehouses/add" element={<WarehousesDetailsAdd />}/> */}
+          <Route path="warehouses/:id/edit" element={<WarehousesDetailsEdit />}/>
+          <Route path="warehouses/add" element={<WarehousesDetailsAdd />}/>
           {/* <Route part='inventory/:id' element={<InventoryDetail />} /> */}
         </Routes>
       </Router>
