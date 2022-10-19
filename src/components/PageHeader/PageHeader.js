1462bbbbd171a16d592ef6cbdea484df54ebe676
J4rdanAG <Jordanarchiegrahn@gmail.com>
2022-10-18 18:01:19 -0700

    added hover, focus states

--- src/components/PageHeader/PageHeader.js
+++ src/components/PageHeader/PageHeader.js
@@ -3,27 +3,30 @@ import instockLogo from '../../assets/images/logo/InStock-Logo.svg'
 import './PageHeader.scss'
 
 export default function PageHeader () {
+
+
+
     return(
 
     
 <div className="Nav">
 
     <div className="Nav__logo">
-    <img src={instockLogo}/>
+    <img src={instockLogo} alt='Instock Logo'/>
     </div>
 
     <div className="Nav__links-container">
 
         <Link className="Nav__link Nav__left">
-            <button>
-                <h3>Warehouses</h3>
+            <button className="Nav__btn">
+                Warehouses
             </button>
             
         </Link>
   
         <Link className="Nav__link">
-            <button>
-                <h3>Inventory</h3>  
+            <button className="Nav__btn">  
+                    Inventory  
             </button>  
         </Link>
    
