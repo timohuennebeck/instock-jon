734525d722102753b355d9385fb69c2973959c2b
Neil Cruz <neilbcruz@gmail.com>
2022-10-20 17:18:45 -0400

    add other form fields as component and add empty value function

--- src/components/AddInventoryForm/AddInventoryForm.js
+++ src/components/AddInventoryForm/AddInventoryForm.js
@@ -4,13 +4,16 @@ import { useState, useRef } from "react";
 import axios from 'axios';
 import InputField from "../../components/InputField/InputField";
 import TextareaField from '../../components/TextareaField/TextareaField';
+import SelectField from '../../components/SelectCategoryField/SelectCategoryField';
+import SelectWareField from '../../components/SelectWareField/SelectWareField';
+import RadioField from '../../components/RadioField/RadioField';
 
 export default function AddInventoryForm() {
     const formValues = useRef();
     const [addInv, setAddInv] = useState([]);
 
     const [errors, setErrors] = useState([]);
-    
+
     const handleSubmit = (event) => {
         event.preventDefault();
 
@@ -63,7 +66,7 @@ export default function AddInventoryForm() {
             })
             .catch((error) => {
                 alert(error.resp.data);
-        })
+            })
     }
 
     return (
@@ -73,113 +76,49 @@ export default function AddInventoryForm() {
                     <div className='addform__details'>
                         <h2 className='addform__title'>Item Details</h2>
                         <InputField
-                            // className='addform__input'
                             label="Item Name"
                             placeholder="Item Name"
                             type='text'
                             name='name'
                             errors={errors}
                         />
-                        {addInv.name === '' && (
-                            <div className='addform__error'>
-                                <img
-                                    className='addform__error-sign'
-                                    src={ErrorSign}
-                                    alt='Red Error Sign for Incomplete Form'
-                                />
-                                <p className='addform__error-text'>This field is required</p>
-                            </div>
-                        )}
                         <TextareaField
                             label='Description'
-                            className='addform__input-description'
                             type='text'
                             name='description'
                             errors={errors}
                         />
-                        <label className='addform__label p-medium' htmlFor='category'>Category</label>
-                        <select className='addform__dropdown'
+                        <SelectField
+                            label='Category'
                             type='text'
                             name='category'
                             errors={errors}
-                        >
-                            <option type="text" value=''>Please Select</option>
-                            <option type='text' value='Electronics'>Electronics</option>
-                            <option type='text' value='Gear'>Gear</option>
-                            <option type='text' value='Apparel'>Apparel</option>
-                            <option type='text' value='Accessories'>Accessories</option>
-                            <option type='text' value='Health'>Health</option>
-                        </select>
-                        {/* {usestate.value(category) === '' && (
-                            <div className='addform__error'>
-                                <img
-                                    className='addform__error-sign'
-                                    src={ErrorSign}
-                                    alt='Red Error Sign for Incomplete Form'
-                                />
-                                <p className='addform__error-text'>This field is required</p>
-                            </div>
-                        )}; */}
+                        />
                     </div>
                     <div className='addform__line'></div>
                     <div className='addform__available'>
                         <h2 className='addform__title'>Item Availability</h2>
-                        <label className='addform__label p-medium'>Status</label>
-                        <div className='addform__status'>
-                            <div className='addform__status-container'>
-                                <input
-                                    className='addform__status-radio'
-                                    type='radio'
-                                    name='status'
-                                    errors={errors}
-                                />
-                                <p className='addform__status-text'>In stock</p>
-                            </div>
-                            <div className='addform__status-container'>
-                                <input
-                                    className='addform__status-radio'
-                                    type='radio'
-                                    name='status'
-                                    errors={errors}
-                                />
-                                <p className='addform__status-text'>Out of stock</p>
-                            </div>
+                        <div>
+                            <RadioField
+                                label='Status'
+                                type='radio'
+                                name='status'
+                                errors={errors}
+                            />
                         </div>
-                        <label className='addform__label p-medium' htmlFor='name'>Quantity</label>
                         <InputField
-                            // className='addform__input'
+                            label='Quantity'
                             type='number'
                             name='quantity'
                             placeholder='0'
                             errors={errors}
                         />
-                        <label className='addform__label p-medium'>Warehouse</label>
-                        <select
-                            className='addform__dropdown'
+                        <SelectWareField
+                            label='Warehouse'
                             type='text'
                             name='warehouse'
                             errors={errors}
-                        >
-                            <option type="text" value=''>Please Select</option>
-                            <option type='text' value='Manhattan'>Manhattan</option>
-                            <option type='text' value='Washington'>Washington</option>
-                            <option type='text' value='Jersey'>Jersey</option>
-                            <option type='text' value='San Fran'>San Fran</option>
-                            <option type='text' value='Santa Monica'>Santa Monica</option>
-                            <option type='text' value='Seattle'>Seattle</option>
-                            <option type='text' value='Miami'>Miami</option>
-                            <option type='text' value='Boston'>Boston</option>
-                        </select>
-                        {/* {usestate.value(warehouse) === '' && (
-                            <div className='addform__error'>
-                                <img
-                                    className='addform__error-sign'
-                                    src={ErrorSign}
-                                    alt='Red Error Sign for Incomplete Form'
-                                />
-                                <p className='addform__error-text'>This field is required</p>
-                            </div>
-                        )}; */}
+                        />
                     </div>
                 </div>
                 <div className='addform__button'>
