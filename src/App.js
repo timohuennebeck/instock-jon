4d06d5f7f76ed36015e54e917b03b3c9b7c976af
Neil Cruz <neilbcruz@gmail.com>
2022-10-19 22:54:57 -0400

    add handlesubmit and placeholder axios

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import './App.scss';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import WarehousesDetailsEdit from './pages/WarehousesDetailsEdit/WarehousesDetailsEdit';
 import WarehousesDetailsAdd from './pages/WarehouseDetailsAdd/WarehousesDetailsAdd';
-// import DeleteNotification from './components/DeleteNotification/DeleteNotification';
-// import PageHeader from './components/PageHeader/PageHeader';
-// import WarehouseList from './pages/WarehouseList/WarehouseList';
+import DeleteNotification from './components/DeleteNotification/DeleteNotification';
+import PageHeader from './components/PageHeader/PageHeader';
+import WarehouseList from './components/WarehouseList/WarehouseList';
 // import WarehouseDetails from './pages/WarehouseDetails/WarehouseDetails';
 // import InventoryList from './pages/InventoryList/InventoryList';
 // import InventoryDetail from './pages/InventoryDetail/InventoryDetail';
@@ -16,15 +16,15 @@ function App() {
     <div>
       <Router>
         {/* <DeleteNotification /> */}
-        {/* <PageHeader /> */}
+        <PageHeader />
         <Routes>
-          {/* <Route path='/' element={<WarehouseList />} /> */}
-          {/* <Route path='/warehouses' element={<WarehouseList />} /> */}
+          <Route path='/' element={<WarehouseList />} />
+          <Route path='/warehouses' element={<WarehouseList />} />
           {/* <Route path='/inventory' element={<InventoryList />} /> */}
           <Route path='/inventory/edit' element={<EditInventory />} />
           <Route path='/inventory/add' element={<AddInventory />} />
           {/* <Route path='/warehouses/:id' element={<WarehouseDetails />} /> */}
-          {/* <Route path="warehouses/:id/edit" element={<WarehousesDetailsEdit />}/> */}
+          <Route path="warehouses/:id/edit" element={<WarehousesDetailsEdit />}/>
           <Route path="warehouses/add" element={<WarehousesDetailsAdd />}/>
           {/* <Route part='inventory/:id' element={<InventoryDetail />} /> */}
         </Routes>
--- src/components/AddInventoryForm/AddInventoryForm.js
+++ src/components/AddInventoryForm/AddInventoryForm.js
@@ -1,38 +1,101 @@
-// import ErrorSign from '../../assets/images/icons/error-24px.svg';
+import ErrorSign from '../../assets/images/icons/error-24px.svg';
 import './AddInventoryForm.scss';
+import { useState, useRef } from "react";
+import axios from 'axios';
+import InputField from "../../components/InputField/InputField";
 
 export default function AddInventoryForm() {
-    // const [errors, setErrors] = useState([]);
+    const formValues = useRef();
+    const [addInv, setAddInv] = useState([]);
+
+    const [errors, setErrors] = useState([]);
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        const errors = [];
+
+        if (!formValues.current.name.value) {
+            errors.push("name");
+        }
+
+        if (!formValues.current.description.value) {
+            errors.push("description");
+        }
+
+        if (!formValues.current.category.value) {
+            errors.push("category");
+        }
+
+        if (!formValues.current.status.value) {
+            errors.push("status");
+        }
+
+        if (!formValues.current.quantity.value) {
+            errors.push("quantity");
+        }
+
+        if (!formValues.current.warehouse.value) {
+            errors.push("warehouse");
+        }
+
+        setErrors(errors)
+
+        const form = event.target;
+
+        const addItemObj = {
+            name: form.name.value,
+            description: form.description.value,
+            category: form.category.value,
+            status: form.status.value,
+            quantity: form.quantity.value,
+            warehouse: form.warehouse.value
+        }
+
+        console.log(addInv);
+        console.log(addItemObj);
+
+        axios
+            .post('http://localhost:8080/inventories', addItemObj)
+            .then((resp) => {
+                setAddInv(addInv(resp.data));
+            })
+            .catch((error) => {
+                alert(error.resp.data);
+            })
+
+    }
     return (
         <>
-            <form className='addform'>
+            <form className='addform' onSubmit={handleSubmit}>
                 <div className='addform__container'>
                     <div className='addform__details'>
                         <h2 className='addform__title'>Item Details</h2>
-                        <label className='addform__label' htmlFor='name'>Item Name</label>
-                        <input
-                            className='addform__input'
+                        {/* <label className='addform__label' htmlFor='itemName'>Item Name</label> */}
+                        <InputField
+                            // className='addform__input'
+                            label="Item Name"
+                            placeholder="Item Name"
                             type='text'
                             name='name'
-                        // value={usestate.value(item-name)}
+                            errors={errors}
                         />
-                        {/* {usestate.value(item-name) === '' && ( */}
-                            {/* <div className='addform__error'>
+                        {addInv.name === '' && (
+                            <div className='addform__error'>
                                 <img
                                     className='addform__error-sign'
                                     src={ErrorSign}
                                     alt='Red Error Sign for Incomplete Form'
                                 />
                                 <p className='addform__error-text'>This field is required</p>
-                            </div> */}
-                        {/* )}; */}
-                        <label className='addform__label' htmlFor='description'>Description</label>
+                            </div>
+                        )}
+                        <label className='addform__label p-medium' htmlFor='description'>Description</label>
                         <textarea
                             className='addform__input-description'
                             type='text'
                             name='description'
-                            // errors={errors}
-                            // value={usestate.value(description)}
+                            errors={errors}
                         />
                         {/* {usestate.value(description) === '' && (
                             <div className='addform__error'>
@@ -44,13 +107,15 @@ export default function AddInventoryForm() {
                                 <p className='addform__error-text'>This field is required</p>
                             </div>
                         )}; */}
-                        <label className='addform__label' htmlFor='category'>Category</label>
+                        <label className='addform__label p-medium' htmlFor='category'>Category</label>
                         <select className='addform__dropdown'
                             type='text'
                             name='category'
-                            // placeholder='Please Select'
+                            errors={errors}
+                            placeholder='Please Select'
                         // value={useState.value(category)} 
                         >
+                            <option type="text" value=''>Please Select</option>
                             <option type='text' value='Electronics'>Electronics</option>
                             <option type='text' value='Gear'>Gear</option>
                             <option type='text' value='Apparel'>Apparel</option>
@@ -71,17 +136,23 @@ export default function AddInventoryForm() {
                     <div className='addform__line'></div>
                     <div className='addform__available'>
                         <h2 className='addform__title'>Item Availability</h2>
-                        <label className='addform__label'>Status</label>
+                        <label className='addform__label p-medium'>Status</label>
                         <div className='addform__status'>
                             <div className='addform__status-container'>
-                                <input className='addform__status-radio' type='radio' name='status'
-                                // value={useState.value(status)}
+                                <input
+                                    className='addform__status-radio'
+                                    type='radio'
+                                    name='status'
+                                    errors={errors}
                                 />
                                 <p className='addform__status-text'>In stock</p>
                             </div>
                             <div className='addform__status-container'>
-                                <input className='addform__status-radio' type='radio' name='status'
-                                // value={useState.value(status)}
+                                <input
+                                    className='addform__status-radio'
+                                    type='radio'
+                                    name='status'
+                                    errors={errors}
                                 />
                                 <p className='addform__status-text'>Out of stock</p>
                             </div>
@@ -96,27 +167,30 @@ export default function AddInventoryForm() {
                                 <p className='addform__error-text'>This field is required</p>
                             </div>
                         )}; */}
-                        <label className='addform__label' htmlFor='name'>Quantity</label>
+                        <label className='addform__label p-medium' htmlFor='name'>Quantity</label>
                         <input
                             className='addform__input'
                             type='number'
                             name='quantity'
                             placeholder='0'
-                        // value={usestate.value(item-name)}
+                            errors={errors}
                         />
-                        <label className='addform__label'>Warehouse</label>
-                        <select className='addform__dropdown' type='text' name='warehouse'
-                        // value={useState.value(warehouse)} 
+                        <label className='addform__label p-medium'>Warehouse</label>
+                        <select
+                            className='addform__dropdown'
+                            type='text'
+                            name='warehouse'
+                            errors={errors}
                         >
-                            <option type='text'>Please Select</option>
-                            <option type='text'>Manhattan</option>
-                            <option type='text'>Washington</option>
-                            <option type='text'>Jersey</option>
-                            <option type='text'>San Fran</option>
-                            <option type='text'>Santa Monica</option>
-                            <option type='text'>Seattle</option>
-                            <option type='text'>Miami</option>
-                            <option type='text'>Boston</option>
+                            <option type="text" value=''>Please Select</option>
+                            <option type='text' value='Manhattan'>Manhattan</option>
+                            <option type='text' value='Washington'>Washington</option>
+                            <option type='text' value='Jersey'>Jersey</option>
+                            <option type='text' value='San Fran'>San Fran</option>
+                            <option type='text' value='Santa Monica'>Santa Monica</option>
+                            <option type='text' value='Seattle'>Seattle</option>
+                            <option type='text' value='Miami'>Miami</option>
+                            <option type='text' value='Boston'>Boston</option>
                         </select>
                         {/* {usestate.value(warehouse) === '' && (
                             <div className='addform__error'>
