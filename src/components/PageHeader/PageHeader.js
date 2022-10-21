05f19ca86540b6f0af0a9e4d4769784704356c56
J4rdanAG <Jordanarchiegrahn@gmail.com>
2022-10-21 10:22:51 -0700

    added padding so components can sit on top, adjusted breakpoint side padding, added link paths

--- src/components/PageHeader/PageHeader.js
+++ src/components/PageHeader/PageHeader.js
@@ -1,43 +1,23 @@
-import { Link } from "react-router-dom"
-import instockLogo from '../../assets/images/logo/InStock-Logo.svg'
-import './PageHeader.scss'
+import { Link } from "react-router-dom";
+import instockLogo from "../../assets/images/logo/InStock-Logo.svg";
+import "./PageHeader.scss";
 
-export default function PageHeader () {
+export default function PageHeader() {
+  return (
+    <div className="Nav">
+      <div className="Nav__logo">
+        <img src={instockLogo} alt="Instock Logo" />
+      </div>
 
-
-
-    return(
-
-    
-<div className="Nav">
-
-    <div className="Nav__logo">
-    <img src={instockLogo} alt='Instock Logo'/>
-    </div>
-
-    <div className="Nav__links-container">
-
-        <Link className="Nav__link Nav__left">
-            <button className="Nav__btn">
-                Warehouses
-            </button>
-            
+      <div className="Nav__links-container">
+        <Link className="Nav__link Nav__left" to='/warehouses'>
+          <button className="Nav__btn">Warehouses</button>
         </Link>
-  
-        <Link className="Nav__link">
-            <button className="Nav__btn">  
-                    Inventory  
-            </button>  
+
+        <Link className="Nav__link" to='/inventory'>
+          <button className="Nav__btn">Inventory</button>
         </Link>
-   
+      </div>
     </div>
-
-    
-   
-
-
-
-
-</div>
-)
+  );
 }
